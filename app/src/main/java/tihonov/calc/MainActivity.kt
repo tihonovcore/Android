e31880112e06468daa52d9b2eaba0f77bcdb3ab3
tihonovcore <tihonovecore@gmail.com>
2018-09-30 15:03:04 +0300

    add horizontal orientation with data saving after turn. add texviews for "( ) . AC". add parse2.jar

--- app/src/main/java/tihonov/calc/MainActivity.kt
+++ app/src/main/java/tihonov/calc/MainActivity.kt
@@ -3,11 +3,16 @@ package tihonov.calc
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import expression.parser.ExpressionParser
+import expression.parser.Parser
 
 class MainActivity : AppCompatActivity() {
     private var expression = "0"
     private lateinit var monitor: TextView
+    private var afterResult: Boolean = false
+    private var afterOperation: Boolean = false
 
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,18 +25,28 @@ class MainActivity : AppCompatActivity() {
         for (curr in digIds.indices) {
             val tmp: TextView = findViewById(digIds[curr])
             tmp.setOnClickListener {
+                if (afterResult) {
+                    expression = ""
+                }
                 print(('0' + curr).toString())
+                afterOperation = false
+                afterResult = false
             }
         }
 
-        val opIds = listOf(R.id.multiply, R.id.divide, R.id.del, R.id.sub, R.id.add, R.id.result)
+        val opIds = listOf(R.id.multiply, R.id.divide, R.id.del, R.id.sub, R.id.add, R.id.result,
+                R.id.clear, R.id.openBrace, R.id.closeBrace, R.id.point)
         val map = mutableMapOf(
                 R.id.multiply to '*',
                 R.id.add to '+',
                 R.id.divide to '/',
                 R.id.del to '<',
                 R.id.sub to '-',
-                R.id.result to '=')
+                R.id.result to '=',
+                R.id.clear to '~',
+                R.id.openBrace to '(',
+                R.id.closeBrace to ')',
+                R.id.point to '.')
 
         for (currId in opIds) {
             val tmp: TextView = findViewById(currId)
@@ -46,16 +61,38 @@ class MainActivity : AppCompatActivity() {
                         }
                         print()
                     }
-                    '=' -> {
-                        //expression = expression.parse()
+                    '~' -> {
+                        expression = "0"
                         print()
                     }
+                    '=' -> {
+                        parse()
+                        afterResult = true
+                        afterOperation = false
+                    }
                     else -> {
+                        if (afterOperation && (expression[expression.length - 1] == '/'
+                                           ||  expression[expression.length - 1] == '*'
+                                           ||  expression[expression.length - 1] == '.')) {
+                            expression = expression.substring(0, expression.length - 1)
+                        }
                         print(map[currId].toString())
+                        afterOperation = true
+                        afterResult = false
                     }
                 }
             }
         }
+
+        if (savedInstanceState != null) {
+            expression = savedInstanceState.getString(EXPR)
+            monitor.text = expression
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(EXPR, expression)
+        super.onSaveInstanceState(outState)
     }
 
     private fun print() {
@@ -75,8 +112,22 @@ class MainActivity : AppCompatActivity() {
 
         print()
     }
-}
 
-fun parse() {
 
+    private fun parse() {
+        var parser: Parser = ExpressionParser()
+        try {
+            var result = parser.parse(expression).evaluate()
+            expression = result.toString()
+            print()
+        } catch (e: Exception) {
+            expression = "0"
+            monitor.text = "ERROR"
+        }
+
+    }
+
+    companion object {
+        private val EXPR = MainActivity::class.java.name + ".expr"
+    }
 }
